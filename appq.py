ec0f5afeef0e8e56a0eef8900cecb11c8aa14a3a
maduartisa <maduart.isa@gmail.com>
2026-05-26 16:26:27 -0300

    atualizado

--- appq.py
+++ appq.py
@@ -4,7 +4,7 @@ from PyQt5.QtCore import Qt
 
 app = QApplication(sys.argv)
 
-label = QLabel("Olá, Turma!")
+label = QLabel("Olá, Turma! Tudo bem?")
 label.resize(300, 200)
 label.setWindowTitle("Minha primeira janela")
 label.setAlignment(Qt.AlignLeft)  # alinha o texto à esquerda
